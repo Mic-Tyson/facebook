97a2ade16c9a14de3af1182a99608e2d426dc70c
manraj <mkooner@live.com>
2024-10-24 14:27:46 +0100

    Add Follow Request relationship

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,3 +1,6 @@
 class Request < ApplicationRecord
-  belongs_to :requester
+  belongs_to :requester, class_name: "User"
+  belongs_to :requested, class_name: "User"
+
+  enum status: { pending: 0, accepted: 1, declined: 2 }
 end
--- db/migrate/20241024131340_create_requests.rb
+++ db/migrate/20241024131340_create_requests.rb
@@ -1,10 +1,15 @@
 class CreateRequests < ActiveRecord::Migration[7.2]
   def change
     create_table :requests do |t|
-      t.references :requester, null: false, foreign_key: true
+      t.references :requester, null: false, foreign_key: { to_table: :users }
+      t.references :requested, null: false, foreign_key: { to_table: :users }
+
       t.integer :status
 
       t.timestamps
     end
+
+    add_index :requests, [ :requester_id, :requested_id ], unique: :true
+    # re-requesting will require separate logic
   end
 end
