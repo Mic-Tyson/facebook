9ddae46648c6c1e9713571ba43f9947d81c0fb42
manraj <mkooner@live.com>
2024-10-24 15:24:13 +0100

    fix dependent typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,5 @@ class User < ApplicationRecord
 
   has_many :posts, foreign_key: "author_id", inverse_of: :author
 
-  has_many :likes, dependant: :destroy
+  has_many :likes, dependent: :destroy
 end
