cb504fdbf40847f63ce9ba09e5bd42c3548af924
manraj <mkooner@live.com>
2024-10-24 17:35:22 +0100

    Add tests for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { should validate_presence_of(:name) }
-  it { should validate_uniqueness_of(:email) }
   it { should have_many(:posts) }
+  it { should have_many(:comments) }
+  it { should have_many(:images) }
+  it { should have_many(:likes) }
+  it { should have_many(:followers).through(:follower_relationships) }
+  it { should have_many(:following).through(:following_relationships) }
+  it { should have_many(:requesters).through(:requester_relationships) }
+  it { should have_many(:requesting).through(:requesting_relationships) }
 end
