637852f25e63c6c89aa51f72ebd8557c0e94ea61
manraj <mkooner@live.com>
2024-10-24 15:31:23 +0100

    Add comments model and migration

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: "User", inverse_of: :posts
 
   has_many :likes, as: :likable, dependant: :destroy
+
+  has_many :comments, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,6 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: "author_id", inverse_of: :author
 
   has_many :likes, dependent: :destroy
+
+  has_many :comments
 end
