3bc5f64932a1424b38f1e4e6950c3f346d4d09a9
Mic-Tyson <mkooner@live.com>
2024-11-04 00:34:12 +0000

    Add search functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,14 @@ class PostsController < ApplicationController
   def show
     @post = Post.find params[:id]
   end
+
+  def search
+    @posts = Post.joins(:author)
+                 .where("title ILIKE :query OR body ILIKE :query OR users.username ILIKE :query", query: "%#{Post.sanitize_sql_like(params[:query])}%")
+
+    respond_to do |format|
+      format.turbo_stream # turbo
+      format.html { render :index } # non-turbo
+    end
+  end
 end
