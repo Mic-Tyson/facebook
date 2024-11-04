ab5ffff3eb4cd0a4dbbdad701bf88ade2fa95e42
Mic-Tyson <mkooner@live.com>
2024-11-04 19:17:01 +0000

    Follow buttons automatically get right action

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def display_card_comments(comments)
     if comments.size > 2
       displayed_comments = comments.first(2).map { |comment| "#{comment.body.truncate(50)} - #{comment.user.username}" }
-      result = displayed_comments.join("<br>") + " ... (#{comments.size - 2} more)"
+      result = displayed_comments.join("<br>") + "<br>" " ... (#{comments.size - 2} more)"
     else
       result = comments.map { |comment| "#{comment.body.truncate(50)} - #{comment.user.username}" }.join("<br>")
     end
@@ -10,24 +10,23 @@ module ApplicationHelper
   end
 
   def display_follow_button(user)
-    return if current_user == user
+    return if user == current_user
 
-    if current_user.following?(user)
-      unfollow_button = button_to "Unfollow",
-        unfollow_user_path(current_user, user_id: user.id),
-        method: :delete,
-        remote: true,
-        class: "like-button"
+    action, path, method = determine_follow_action(user)
+    button = button_to(action, path, method: method, remote: true, class: "like-button")
 
-      content_tag(:div, unfollow_button, class: "follow-button-container", id: "follow_button_#{user.id}")
+    content_tag(:div, button, class: "follow-button-container", id: "follow_button_#{user.id}")
+  end
+
+  private
+
+  def determine_follow_action(user)
+    if current_user.requesting?(user)
+      [ "Unrequest", unrequest_follow_user_path(user), :delete ]
+    elsif current_user.following?(user)
+      [ "Unfollow", unfollow_user_path(user), :delete ]
     else
-      follow_button = button_to "Follow",
-        follow_user_path(current_user, user_id: user.id),
-        method: :post,
-        remote: true,
-        class: "like-button"
-
-      content_tag(:div, follow_button, class: "follow-button-container", id: "follow_button_#{user.id}")
+      [ "Request", request_follow_user_path(user), :post ]
     end
   end
 end
