c42fa8773c874c2d69819634673dafffb679aa86
Mic-Tyson <mkooner@live.com>
2024-11-06 17:01:57 +0000

    Update build script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,6 +5,8 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+bundle exec rails db:create
+bundle exec rails db:migrate
 
 # If you're using a Free instance type, you need to
 # perform database migrations in the build command.
